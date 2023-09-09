523a84a5850fd9fb8203df34d204631910aaa216
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-09-09 16:42:37 +0200

    stylize the form and the table

--- resources/views/admin/project/edit.blade.php
+++ resources/views/admin/project/edit.blade.php
@@ -2,8 +2,8 @@
 @section('title', 'Create Post')
 
 @section('content')
-    <header class="d-flex justify-content-between align-items-center">
-        <h1>modifica il Post</h1>
+    <header class="d-flex justify-content-between align-items-center container">
+        <h4>MODIFICA IL POST</h4>
         <a href="{{ route('admin.project.index') }}" class="btn btn-outline-secondary">
             <i class="fas fa-arrow-left me-2"></i>
             torna indietro
